a268f1595aa87559606d0134fe7811dd2d362218
Chad Welch <cwelc001@gmail.com>
2023-10-10 17:25:22 -0700

    add 5 min max playtime

--- src/app/youtube/page.tsx
+++ src/app/youtube/page.tsx
@@ -1,29 +1,27 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import YouTube, { YouTubeProps } from "react-youtube";
 
 export default function YouTubePlayer() {
-  const [nowPlaying, setNowPlaying] = useState<string | null>("JTBJ3tW2Lr0");
-  const [queue, setQueue] = useState<string[]>(["xzpndHtdl9A", "2g811Eo7K8U"]);
+  // const [nowPlaying, setNowPlaying] = useState<string | null>("JTBJ3tW2Lr0");
+  const [nowPlaying, setNowPlaying] = useState<string | null>("4ASKMcdCc3g");
+  // const [queue, setQueue] = useState<string[]>(["xzpndHtdl9A", "2g811Eo7K8U"]);
+  const [queue, setQueue] = useState<string[]>(["xzpndHtdl9A", "4ASKMcdCc3g"]);
 
   const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    // access to player in all event handlers via event.target
-    // event.target.pauseVideo();
+    event.target.playVideo();
   };
 
-  const onPlayerStateChange: YouTubeProps["onStateChange"] = (event) => {
-    console.log("event", event);
-    if (event.data === 0) {
-      if (!queue) {
-        setNowPlaying(null);
-        return;
-      }
-
-      setNowPlaying(queue[0]);
-      setQueue((prev) => {
-        return [...prev.slice(1)];
-      });
+  const startNextVideo = () => {
+    if (!queue) {
+      setNowPlaying(null);
+      return;
     }
+
+    setNowPlaying(queue[0]);
+    setQueue((prev) => {
+      return [...prev.slice(1)];
+    });
   };
 
   const opts = {
@@ -32,6 +30,7 @@ export default function YouTubePlayer() {
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
+      end: 300,
     },
   };
 
@@ -42,7 +41,7 @@ export default function YouTubePlayer() {
           videoId={nowPlaying}
           opts={opts}
           onReady={onPlayerReady}
-          onStateChange={onPlayerStateChange}
+          onEnd={() => startNextVideo()}
         />
       )}
     </>
