c76cba6a29ffe294edbab351edd15df5dee6ad29
Chad Welch <cwelc001@gmail.com>
2023-10-25 13:44:15 -0700

    more youtube queue display formatting

--- src/app/youtube/queue/videoDisplay.tsx
+++ src/app/youtube/queue/videoDisplay.tsx
@@ -15,25 +15,25 @@ export default function VideoDisplay({
 
   // TODO: More formatting, add zap amount
   return (
-    <div className="flex justify-between text-white py-1 lg:w-1/2 items-center">
-      <div className="flex gap-2 items-center">
+    <div className="flex justify-between text-white py-1 items-center">
+      <div className="flex gap-2 items-center max-w-1/3">
         {profile?.picture && (
           <img
             className="h-10 w-10 shrink-0 rounded-[50%]"
             src={profile.picture}
           />
         )}
-        <p className="w-full text-xl font-semibold">
-          {fmtMsg(displayName(video.pubkey, profile), 20)}
+        <p className="w-full text-xl font-semibold text-ellipsis max-w-1/3">
+          {fmtMsg(displayName(video.pubkey, profile), 15)}
         </p>
       </div>
       <div className="flex gap-x-2 items-center">
         <div>
-          <h1 className="text-xl font-semibold text-right">
-            {fmtMsg(video.title, 30)}
+          <h1 className="text-xl font-semibold text-right text-ellipsis whitespace-nowrap">
+            {fmtMsg(video.title, 20)}
           </h1>
-          <h3 className="text-lg text-gray-400 text-right">
-            {fmtMsg(video.author, 30)}
+          <h3 className="text-lg text-gray-400 text-right text-ellipsis whitespace-nowrap">
+            {fmtMsg(video.author, 20)}
           </h3>
         </div>
         <img className="h-12 w-12 rounded" src={video.thumbnail} />
