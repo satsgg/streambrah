6283ba7dd964e23a3cfae85713c1387d5603aa20
Chad Welch <cwelc001@gmail.com>
2023-12-12 08:07:20 -0800

    clean rest of svgs

--- src/app/streamManager/removeableParticipantForm.tsx
+++ src/app/streamManager/removeableParticipantForm.tsx
@@ -1,4 +1,5 @@
 import UserDisplay from "./userDisplay";
+import RemoveParticipantSVG from "@/svgs/removeParticipant.svg";
 
 export default function RemoveableParticipantForm({
   participant,
@@ -11,22 +12,12 @@ export default function RemoveableParticipantForm({
     <div className="flex gap-2 justify-between">
       <UserDisplay pubkey={participant} />
       <button onClick={() => removeParticipant(participant)}>
-        <svg
-          width={20}
+        <RemoveParticipantSVG
           height={20}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
+          width={20}
           strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
-          />
-        </svg>
+          alt="Remove participant"
+        />
       </button>
     </div>
   );
